ab3e2f49813246f93e86d8c20dce19c1c8f1b007
Isabel <isabelpastorlopezcv@gmail.com>
2022-09-25 21:38:13 +0200

    ejercicios en casa

--- U1/ejercicio2/js/ejercicio.js
+++ U1/ejercicio2/js/ejercicio.js
@@ -1,2 +1,30 @@
-//Modifica el programa anterior: si los números no son un número o son menores o iguales a
-//ceros, que los vuelva a pedir.
+/*
+ * Modifica el programa anterior: si los números no son un número o son menores o iguales a
+ * ceros, que los vuelva a pedir.
+ */
+
+function isGreaterThan(num1, num2) {
+  num1 > num2
+    ? alert(`El número ${num1} es mayor que ${num2}.`)
+    : alert(`El número ${num1} es menor que ${num2}.`);
+}
+function compareNumbers(num1, num2) {
+  num1 == num2
+    ? alert(`El número ${num1} es igual que ${num2}.`)
+    : isGreaterThan(num1, num2);
+}
+function checkNumber(num) {
+  while ( isNaN(num) || num <= 0 ) { 
+    // que coincida el tipo y que sea mayor que 0
+    num = prompt("Vuelva a introducir el número (Número entero positivo");
+  }
+  return num;
+}
+
+// Comprobar funcionamiento
+let num1, num2;
+num1 = prompt("Introduzca un número");
+num1 = checkNumber(num1);
+num2 = prompt("Introduzca otro número");
+num2 = checkNumber(num2);
+compareNumbers(num1, num2);
--- U1/ejercicio3/js/ejercicio.js
+++ U1/ejercicio3/js/ejercicio.js
@@ -1,1 +1,37 @@
 //Dada una hora en horas, minutos y segundos, indicar qué hora será pasado un segundo.
+let hour, min, sec, timeToAdd; // hora, minutos, segundos, tiempo a añadir
+
+function checkNumber(num) {
+  while (isNaN(num) || num < 0) {// mientras no sea número o sea menor de 0 
+    num = prompt(`Introduzca un número entero positivo`);
+    // assert
+    alert(`chequeando que se ha introducido bien el número por el prompt : ${num}`)
+  }
+  timeToAdd = Math.trunc(num/max) 
+  // assert
+  alert(`chequeando que el tiempo a añadir a los minutos es correcto : ${timeToAdd}`)
+
+  //assert
+  alert(`chequeando que el resultado que va a devolver el chequeador es correcto : ${num%max}`)
+
+  return num % max
+}
+
+function setSecond(seconds) {
+  timeToAdd = 0
+  max = 60;
+  sec = prompt(`Introduzca los segundos`);
+  sec = checkNumber(sec);
+  alert(sec)
+}
+
+function setMinut(minutes, minutesAdded) {
+  max = 60;
+  min = prompt(`Introduzca los minutos`);
+  min =+ timeToAdd
+  min = checkNumber(min);
+  alert(min)
+}
+
+setSecond()
+setMinut()
