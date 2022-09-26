87e5cc3b2fd66606b7b24500296fdf29c393d917
Isabel <isabelpastorlopezcv@gmail.com>
2022-09-26 20:57:33 +0200

    commit

--- U1/ejercicio5/js/ejercicio.js
+++ U1/ejercicio5/js/ejercicio.js
@@ -1,1 +1,14 @@
 //Mostrar todos los números que hay entre dos números introducidos por el usuario.
+function countNumbersBetween(num1, num2){
+    for (let i = num1+1 ; i < num2 ; i++) {
+        alert(`${i}`)
+    }
+}
+
+
+function putNumber(){
+    let num = Math.floor(prompt(`Introduzca un número`))
+    return num
+}
+
+countNumbersBetween(putNumber(), putNumber())
