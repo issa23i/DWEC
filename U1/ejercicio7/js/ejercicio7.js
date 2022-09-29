f4248d2e5aad03fe2fc2e691ed1c7c531bd77a11
issa23i <ipaslop262@iesmartinezm.es>
2022-09-29 09:48:31 +0200

    commit clase

--- U1/ejercicio7/js/ejercicio7.js
+++ U1/ejercicio7/js/ejercicio7.js
@@ -1,13 +1,13 @@
 // 7. Mostrar todos los números divisores de un número introducido por el usuario.
 'use strict';
-let num, aux
-num = parseInt(prompt("Introduzca un número"))
-
-num<0 ? aux = -num : aux = num // aux = num, si es negativo se pasa a positivo
-
-while( aux > 0 ){
-    if (num%aux===0){
-        console.log(aux);
+let n;
+ 
+n = parseInt(prompt("Indica el numero: "));
+ 
+ 
+for (let i = 2; i < n; i++) {
+    if (n%i===0) {
+        console.log(i);
     }
-    aux--
+ 
 }
