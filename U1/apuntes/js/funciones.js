dff1d9da7259f80a5333dd386eb235264a7706e9
issa23i <ipaslop262@iesmartinezm.es>
2022-09-29 11:01:29 +0200

    commit clase

---
+++ U1/apuntes/js/funciones.js
@@ -0,0 +1,6 @@
+//rest
+
+function pruebaParREST(a, b, ...masParametros) {
+    console.log("a: "+a+" b: "+ b + " otros: " + masParametros);
+    }
+    pruebaParREST("param1", "param1", "param3", "param4", "param5");
