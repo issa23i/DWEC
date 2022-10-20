6962ec0dfb8166c6e2e788000d84120ec68f7d52
Isabel <isabelpastorlopezcv@gmail.com>
2022-10-20 17:40:33 +0200

    aprendido par,impar

--- U1/apuntes/js/stringspruebas.js
+++ U1/apuntes/js/stringspruebas.js
@@ -7,3 +7,6 @@ let palabra3 = '8aesaaaaaa'
 let nuevoArray = palabra1.split('').filter( l => l==='a')
 console.log(nuevoArray); // 'aaaaa'
 console.log(nuevoArray.length); // 5
+
+/// Así sabemos si es par o impar, par devuelve 0, impar devuelve 1
+console.log(6 & 1);
