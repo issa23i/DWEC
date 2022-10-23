e1e9a5c5f0b958aa2956bdfa1b47f750a8218e43
Isabel <isabelpastorlopezcv@gmail.com>
2022-10-23 18:12:01 +0200

    ejercicio5 finalizado en casa

--- U1/NumeroPatrones/js/javascript.js
+++ U1/NumeroPatrones/js/javascript.js
@@ -9,25 +9,23 @@
 // function numeroPatrones(texto)
 // que devolverá un número entero con el número de patrones encontrados.
 
-const normalize = (txt) => {
-    return txt.toLowerCase().replace(",","").replace(".","").replace("¡","").replace("!","").replace("¿","").replace("?", "")
-}
-
 const numeroPatrones = (txt) => {
-    let str = normalize(txt).split(" ") // split separa y hace un array, en este caso con el separador " "
     let regex1, regex2, regex3, regex4
     regex1 = new RegExp(/00/gi)
     regex2 = new RegExp(/101/gi)
     regex3 = new RegExp(/ABC/gi)
     regex4 = new RegExp(/HO/gi)
     let regexs = [regex1,regex2,regex3,regex4]
-    let contador = 0
+    let arrayResult = []
     for (const r of regexs) {
-        for (const l of str) {
-            if (r.test(l)) {contador++}
+        let texto = txt
+        contador = 0
+        while(texto.search(r)>=0){
+            texto = texto.substring(texto.search(r)+1)
+            contador++
         }
+        arrayResult.push({expresionRegular:r,coincidencias:contador})
     }
-    return contador
+    return arrayResult
 }
-
-console.log(numeroPatrones("1000 101 llHOll 3ABC 4A BC00"));
+console.log(numeroPatrones("1000 101 llHOll 3ABC 4A BC000 00 101"));
