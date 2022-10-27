97e84f2e6694232a646f2493f5fbaac27dd0c699
Isabel <isabelpastorlopezcv@gmail.com>
2022-10-27 16:21:53 +0200

    ejercicio ipasen en casa

--- U1/usuariosIPasen/js/javascript.js
+++ U1/usuariosIPasen/js/javascript.js
@@ -7,9 +7,20 @@ let alumnos = [
 ]
 let arrayResultante = []
 const toIPasen = (alumnos) => {
+    
     alumnos.map(alumno => {
-        let ipasen = alumno["nombre"].split(" ")
-        ipasen.push(alumno["dni"])
-        ipasen[0] = ipasen[0].slice(0,1)
+
+        let ipasen = (alumno["nombre"].split(" "))
+        ipasen.push(alumno["dni"]) // array con nombre,apellido,apellido2,dni
+        ipasen[0] = ipasen[0].slice(0,1) // primera letra del nombre
+        ipasen[1] = ipasen[1].slice(0,3) // tres primeras letras del apellido1
+        ipasen[2] = ipasen[2].slice(0,3) // tres primeras letras del apellido2
+        ipasen[3] = ipasen[3].slice(5,8) // tres últimos dígitos del dni (sin la letra)
+        ipasen = ipasen.toString().replaceAll(",","").toLowerCase()
+        const {nombre, dni, iPasen} = alumno
+        alumno["iPasen"] = ipasen
     })
+    return alumnos
 }
+
+console.log(toIPasen(alumnos));
