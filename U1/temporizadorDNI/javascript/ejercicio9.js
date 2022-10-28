1b0be1788f2a881a6138212e4acf4864b46c1974
issa23i <ipaslop262@iesmartinezm.es>
2022-10-28 13:56:20 +0200

    jercicio clase

--- U1/temporizadorDNI/javascript/ejercicio9.js
+++ U1/temporizadorDNI/javascript/ejercicio9.js
@@ -15,8 +15,8 @@ const pushDni = () => {
     do{
         dni = prompt('Inserte un dni sin letra (8 digitos)')
         if(regex.test(dni)){
+            dni = dni.concat(letra(dni))
             dnis.push(dni)
-
         }
     } while (dni!=='-1')
     
@@ -30,9 +30,8 @@ const pushDni = () => {
 }
 
 const letra = (dni) =>{
-    restos= [	0,	1,	2,	3,	4,	5,	6,	7,	8,	9,	10,	11,	12,	13,	14,	15,	16,	17,	18,	19,	20,	21,	22]
     control=	[T,	R,	W,	A,	G,	M,	Y,	F,	P,	D,	X,	B,	N,	J,	Z,	S,	Q,	V,	H,	L,	C,	K,	E]
-    dni
+    return control[dni%23]
 
 }
 
