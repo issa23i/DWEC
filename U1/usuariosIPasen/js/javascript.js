29a9ea361515be3f8751914640233d821ba34f13
Isabel <isabelpastorlopezcv@gmail.com>
2022-11-02 22:06:08 +0100

    estudio en casa

--- U1/usuariosIPasen/js/javascript.js
+++ U1/usuariosIPasen/js/javascript.js
@@ -5,21 +5,24 @@ let alumnos = [
     {nombre: "Mariano Benavent Castaño", dni: "28555222Z"},
     {nombre: "Angeles Castro Quintana", dni: "66777888L"}
 ]
-let arrayResultante = []
+
+
+const stringToIPasen = alumno => {
+    let array = alumno["nombre"].split(" ") // Dividir nombre, apellido1 y apellido2 en tres strings dentro de un array
+    array.push(alumno["dni"]) // Se le añade la cadena dni
+    array[0] = array[0].slice(0,1) // primera letra del nombre
+    array[1] = array[1].slice(0,3) // tres primeras letras del apellido1
+    array[2] = array[2].slice(0,3) // tres primeras letras del apellido2
+    array[3] = array[3].slice(5,8) // tres últimos dígitos del dni (sin la letra)
+    return array.join("").toLowerCase()
+}
+
+
 const toIPasen = (alumnos) => {
     
     alumnos.map(alumno => {
-
-        let ipasen = (alumno["nombre"].split(" "))
-        ipasen.push(alumno["dni"]) // array con nombre,apellido,apellido2,dni
-        ipasen[0] = ipasen[0].slice(0,1) // primera letra del nombre
-        ipasen[1] = ipasen[1].slice(0,3) // tres primeras letras del apellido1
-        ipasen[2] = ipasen[2].slice(0,3) // tres primeras letras del apellido2
-        ipasen[3] = ipasen[3].slice(5,8) // tres últimos dígitos del dni (sin la letra)
-        ipasen = ipasen.toString().replaceAll(",","").toLowerCase()
-        alumno["iPasen"] = ipasen
+        alumno["iPasen"] = stringToIPasen(alumno)
     })
     return alumnos
 }
-
 console.log(toIPasen(alumnos));
