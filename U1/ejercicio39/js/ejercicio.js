8d5c8da9a2a0adaaa9b87f409107a9c695bd9928
Isabel <isabelpastorlopezcv@gmail.com>
2022-11-02 16:37:53 +0100

    entendiendo ejercicio 38

--- U1/ejercicio39/js/ejercicio.js
+++ U1/ejercicio39/js/ejercicio.js
@@ -1,10 +1,11 @@
 "use strict";
 // 39. Programa una función que dado un array de elementos, elimine los duplicados, pe.e mi
 // Funcion (“x”,10,”x”,2,”10,10, true,true) devolverá [“x”,10,2,”10”,true]
-
+// filter (elemento, indice) => array.indexOf(elemento) === indice
+// si está repetido, la primera vez será verdadero, pero las próximas ya no
 const removeDuplicates = array => array.filter( (n,m) => array.indexOf(n) === m) 
 const array = ['x',10,'x',2,'10',10, true,true]
-// console.log(removeDuplicates(array));
+ console.log(removeDuplicates(array));
 // OTRA FORMA ES CON SET
 // cpmst sinDuplicados = (elementes = undefined) => [...new Set (elements)];
 let conjunto = new Set(array)
