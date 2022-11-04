cb45e5e94ae3cbc8f902e84adb90507e6ffe9966
Isabel <isabelpastorlopezcv@gmail.com>
2022-11-04 22:17:53 +0100

    estudiar

--- U2/apuntes/js/dom.js
+++ U2/apuntes/js/dom.js
@@ -111,6 +111,32 @@ const div = document.createElement("div"); // Creamos un <div></div>
 const span = document.createElement("span"); // Creamos un <span></span>
 const img = document.createElement("img"); // Creamos un <img>
 const comment = document.createComment("Comentario"); // <!--Comentario-->
+// clonar un elemento
+div.textContent = "Elemento 1";
+const div2 = div.cloneNode(); // Ahora SÍ estamos clonando
+div2.textContent = "Elemento 2";
+/**El método cloneNode(deep) acepta un parámetro boolean deep opcional, a false por defecto, para
+indicar el tipo de clonación que se realizará:
+• Si es true, clonará también sus hijos, conocido como una clonación profunda (Deep Clone).
+• Si es false, no clonará sus hijos, conocido como una clonación superficial (Shallow Clone). */
+div2.cloneNode(true) // clona sus hijos también
+div2.textContent = "Elemento 2 con hijos del elemento 1";
+
+
+// atributos
+//Ejemplo 1
+const div = document.createElement("div"); // <div></div>
+div.id = "page"; // <div id="page"></div>
+div.className = "data"; // <div id="page" class="data"></div>
+div.style = "color: red"; // <div id="page" class="data" style="color: red"></div>
+
+// hasAttribute
+// Obtenemos <div id="page" class="info data dark" data-number="5"></div>
+const div = document.querySelector("#page");
+div.hasAttribute("data-number"); // true (data-number existe)
+div.hasAttributes(); // true (tiene 3 atributos)
+div.getAttributeNames(); // ["id", "data-number", "class"]
+div.getAttribute("id"); // "page"
 
 
 
@@ -120,5 +146,77 @@ const $d = document
 const buttonEvent = $d.querySelector("#evento-atributo")
 const funcionEvento = () => alert("Hola Mundo")
 buttonEvent.addEventListener("click", funcionEvento)
+div.removeAttribute("id"); // class="info data dark" y data-number="5"
+div.setAttribute("id", "page"); // Vuelve a añadir id="page"
 
+// atributo data
+const $linkDOM = document.querySelector(".link-dom");
+console.log($linkDOM.getAttribute("data-description"));
+console.log($linkDOM.dataset);
+console.log($linkDOM.dataset.description);
+$linkDOM.setAttribute("data-description", "Modelo de Objeto del Documento");
+console.log($linkDOM.dataset.description);
+$linkDOM.dataset.description = "Suscríbete a mi canal y comparte";
+console.log($linkDOM.dataset.description);
+console.log($linkDOM.hasAttribute("data-id"));
+$linkDOM.removeAttribute("data-id");
+console.log($linkDOM.hasAttribute("data-id"));
 
+// estilos
+console.log($linkDOM.style);//Muestra valores asignados por el usuarios
+console.log($linkDOM.getAttribute("style"));
+console.log($linkDOM.style.backgroundColor);
+console.log($linkDOM.style.color);
+$linkDOM.style.setProperty("text-decoration", "none");
+$linkDOM.style.setProperty("display", "block");
+$linkDOM.style.width = "50%";
+$linkDOM.style.textAlign = "center";
+$linkDOM.style.marginLeft = "auto";
+$linkDOM.style.marginRight = "auto";
+$linkDOM.style.padding = "1rem";
+$linkDOM.style.borderRadius = ".5rem";
+console.log($linkDOM.style);
+console.log($linkDOM.getAttribute("style"));
+console.log(getComputedStyle($linkDOM));
+//Variables CSS - Custom Properties CSS
+const $html = document.documentElement,
+$body = document.body;
+let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color"),
+varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color");
+console.log(varDarkColor, varYellowColor);
+$body.style.backgroundColor = varDarkColor;
+$body.style.color = varYellowColor;
+$html.style.setProperty("--dark-color", "#000");
+varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color");
+$body.style.setProperty("background-color", varDarkColor);
+
+// API classList .className y .classList
+const div = document.querySelector(".element");
+// Obtener clases CSS
+div.className; // "element shine dark-theme"
+div.getAttribute("class"); // "element shine dark-theme"
+// Modificar clases CSS
+div.className = "elemento brillo tema-oscuro";
+div.setAttribute("class", "elemento brillo tema-oscuro");
+
+// .classList
+const div = document.querySelector("#page");
+div.classList; // ["info", "data", "dark"]
+div.classList.add("uno", "dos"); // No devuelve nada.
+div.classList; // ["info", "data", "dark", "uno", "dos"]
+div.classList.remove("uno", "dos"); // No devuelve nada.
+div.classList; // ["info", "data", "dark"]
+const div = document.querySelector("#page");
+div.classList; // ["info", "data", "dark"]
+div.classList.toggle("info"); // Como "info" existe, lo elimina. Devuelve "false"
+div.classList; // ["data", "dark"]
+div.classList.toggle("info"); // Como "info" no existe, lo añade. Devuelve "true"
+div.classList; // ["info", "data", "dark"]
+const div = document.querySelector("#page");
+div.classList; // ["info", "data", "dark"]
+div.classList.item(1); // 'data'
+div.classList.contains("info"); // Devuelve `true` (existe la clase)
+div.classList.replace("dark", "light"); // Devuelve `true` (se hizo el cambio)
+
+// reemplazar contenido
+(página 16 de los apuntes del tema 2)
