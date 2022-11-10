c11a57afe56e8be541edea77593c975f0bccd44d
Isabel <isabelpastorlopezcv@gmail.com>
2022-11-10 22:05:43 +0100

    ejercicio1 a medias

--- U2/ejercicio1/js/script.js
+++ U2/ejercicio1/js/script.js
@@ -17,16 +17,6 @@ for (const boton of botones) {
 }
 const $btnNuevoNumero = $btn
 
-const $btnNuevoBoton = $d.querySelector('[name="boton"]')
-
-const addButton = () => {
-    $newButton = $btnNuevoBoton.cloneNode()
-    $newButton.textContent = $btnNuevoBoton.value
-    $btnNuevoBoton.insertAdjacentElement("afterend",$newButton)
-}
-
-$btnNuevoBoton.addEventListener("click",addButton)
-
 // color random
 const setRandomColor = () => `rgb(${parseInt(Math.random()*255)},${parseInt(Math.random()*255)},${parseInt(Math.random()*255)})`
 
@@ -43,6 +33,24 @@ const addUl = () => {
     $itemListaDesordenada.style.backgroundColor=setRandomColor()
 }
 $btnNuevoNumero.addEventListener("click",addUl)
+
+
+///////////////// crear botón con el número random y luego mostrar un alert con el número al pulsar el botón
+const $btnNuevoBoton = $d.querySelector('[name="boton"]')
+
+const addButton = () => {
+    const $newButton = $d.createElement('button')
+    let random = parseInt(Math.random()*(100))
+    $newButton.textContent = random
+    $btnNuevoBoton.insertAdjacentElement("afterend",$newButton)
+    console.log($newButton.setAttribute('name', random))
+    $newButton.addEventListener("click", showRandom)
+}
+const showRandom = () => {
+    alert(this.getAttribute('name'))
+}
+
+$btnNuevoBoton.addEventListener("click",addButton)
 
 
 
