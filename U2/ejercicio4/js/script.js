36260af4a99c91fadaa61f84bd3a8ba854e5efff
issa23i <ipaslop262@iesmartinezm.es>
2022-11-10 12:01:32 +0100

    ejeercicio 4

--- U2/ejercicio4/js/script.js
+++ U2/ejercicio4/js/script.js
@@ -3,3 +3,24 @@
 // También deberá tener un botón “Reaparecer” que hará que aparezcan todos los
 // elementos desaparecidos (pero no los eliminados).
 
+const $d = document;
+const $body = $d.body;
+
+const hideDelete = () => {
+  // Array de los tres $p
+  const pes = $d.querySelectorAll('p');
+  console.log(pes);
+  for (const p of pes) {
+    p.addEventListener("mouseout", pHidden);
+    p.addEventListener("click", pDelete);
+  }
+};
+
+const pDelete = () => {
+  this.remove();
+};
+
+const pHidden = () => {
+  this.classList = ["oculto"];
+};
+hideDelete();
