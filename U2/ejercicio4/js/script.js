94fe926058e9118086b82b28383360e7858f0df1
issa23i <ipaslop262@iesmartinezm.es>
2022-11-10 11:07:18 +0100

    organizar copia ejercicio

---
+++ U2/ejercicio4/js/script.js
@@ -0,0 +1,5 @@
+// 4. Realiza un programa que tenga 3 elementos <p> y al hacer salir el puntero del ratón
+// sobre ellos desaparezcan (se oculten) y al hacer doble clic (los elimine del DOM).
+// También deberá tener un botón “Reaparecer” que hará que aparezcan todos los
+// elementos desaparecidos (pero no los eliminados).
+
