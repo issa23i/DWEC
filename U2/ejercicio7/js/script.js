a1cca57179a1ea57c5ea3180308a2fdeca126a77
issa23i <ipaslop262@iesmartinezm.es>
2022-11-14 12:58:09 +0100

    combined

--- U2/ejercicio7/js/script.js
+++ U2/ejercicio7/js/script.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-/** 
- * 7. Realiza un programa que cuando se presione una tecla, calcule cuántos DNIs de 4 cifras
-(del 0001 al 9999) tienen esa letra y te mostrará los DNIs y sus letras en un elemento
-HTML(Ejemplo un <p> y una lista desplegable) .
- explicación --->
- * parrafo "listado de dni validos con la letra (letra pulsada)"
- * desplegable con la lista de los dnis
- */
-=======
+
 /**
  * 7. Realiza un programa que cuando se presione una tecla, calcule cuántos DNIs de 4 cifras
 (del 0001 al 9999) tienen esa letra y te mostrará los DNIs y sus letras en un elemento
