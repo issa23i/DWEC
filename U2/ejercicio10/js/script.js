f772cd92b68c28d65405a1bc83b3edd2b2d79d03
issa23i <ipaslop262@iesmartinezm.es>
2022-11-17 11:28:40 +0100

    hacer jeecicio 10

---
+++ U2/ejercicio10/js/script.js
@@ -0,0 +1,8 @@
+/**
+ * 10. Realiza un formulario donde se pueda introducir y enviar un DNI con letra. El formulario
+deberá validar si la letra es correcta al:
+a) Perder el foco del campo de texto donde se introduce el DNI.
+b) Enviar el formulario, cancelando el envío si el formato no es correcto.
+ */
+
+
