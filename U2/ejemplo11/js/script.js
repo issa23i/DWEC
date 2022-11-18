144ba7fb58cdf1812e55b355d8d1b2ea3ab1b022
issa23i <ipaslop262@iesmartinezm.es>
2022-11-18 13:33:56 +0100

    ejercicio11

---
+++ U2/ejemplo11/js/script.js
@@ -0,0 +1,31 @@
+// 11. Realiza un formulario que pida una dirección de email y la valide antes de enviarla:
+// a) Debe validar si el email sigue el formato texto@servidor.loquesea
+// b) Además de validar el formato anterior, debe comprobar que servidor.loquesea este
+// como servidor admitido en un array de servidores llamado “listaServidores”.
+// Dicho array debe ser definido a mano en el código.
+
+// Ejemplo
+const $d = document
+const $input = $d.querySelector('#email')
+const $btn = $d.querySelector('button')
+
+const validar = (e) => {
+    let exreg = new RegExp(/\d+\w*@\d*\w*\.\d*\w*/)
+    if(!exreg.test($input.value) || !validarServidor($input.value)){
+        e.preventDefault()
+        alert("Email no válido")
+    }
+}
+
+const validarServidor = (email) => {
+    let servidor = (email.split('@'))[1]
+    console.log(servidor);
+    let listaServidores=["terra.es","google.com","marca.es","yahoo.es","iesmartinezm.es"];
+    return listaServidores.find(s => s === servidor)
+}
+
+$btn.addEventListener('click',validar)
+
+
+
+
