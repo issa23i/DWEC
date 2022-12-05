ece7da70934bc9429ff5cb1146e66ec4fd2e8844
issa23i <ipaslop262@iesmartinezm.es>
2022-12-05 09:02:47 +0100

    tema 3

---
+++ U3/ejercicio0/js/script.js
@@ -0,0 +1,6 @@
+/**
+ * Realiza una Web que almacene en una cookie el número de visitas que has realizado (cada vez que se carga el sitio, cuenta como una visita), incrementado el valor del número de visitas a cada visita realizada. La web debe tener una interfaz para visualizar el contenido de la cookie y para eliminar la cookie.
+Después realiza el mismo ejercicio almacenando el número de visitas con LocalStorage y SessionStorage.
+
+ */
+
