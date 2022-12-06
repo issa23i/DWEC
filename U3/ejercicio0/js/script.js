1a1078a9369ebc31d323dbbd49c25bf088dcc9c3
Isabel <isabelpastorlopezcv@gmail.com>
2022-12-06 18:55:32 +0100

    ejercicio cookies

--- U3/ejercicio0/js/script.js
+++ U3/ejercicio0/js/script.js
@@ -4,3 +4,45 @@ Después realiza el mismo ejercicio almacenando el número de visitas con LocalS
 
  */
 
+
+function checkCookie() {
+    let contador = getCookie("contador");
+    if (contador != "") {
+      contador = parseInt(contador) +1
+      setCookie('contador', contador, 365)
+      alert("Esta es su " + contador + "ª visita");
+    } else {
+      contador = 0;
+      if (contador !== "" && contador !== null) {
+        setCookie("contador", contador, 365);
+      }
+    }
+  }
+  
+
+window.addEventListener("load", checkCookie);
+
+
+function setCookie(cname, cvalue, exdays) {
+  let d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
