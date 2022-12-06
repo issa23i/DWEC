e8e174a47ce14a6d0dd5e3bcd42f88f95a7e87c9
Isabel <isabelpastorlopezcv@gmail.com>
2022-12-06 19:17:47 +0100

    Ejercicio completo realizado cookies

--- U3/ejercicio0/js/script.js
+++ U3/ejercicio0/js/script.js
@@ -4,24 +4,28 @@ Después realiza el mismo ejercicio almacenando el número de visitas con LocalS
 
  */
 
+///// COOKIE /////////////////////////////////
+
+const p1 = document.querySelector("#cookie");
+const p2 = document.querySelector("#localstorage");
+const p3 = document.querySelector('#sessionstorage')
 
 function checkCookie() {
-    let contador = getCookie("contador");
-    if (contador != "") {
-      contador = parseInt(contador) +1
-      setCookie('contador', contador, 365)
-      alert("Esta es su " + contador + "ª visita");
-    } else {
-      contador = 0;
-      if (contador !== "" && contador !== null) {
-        setCookie("contador", contador, 365);
-      }
+  let contador = getCookie("contador");
+  if (contador != "") {
+    contador = parseInt(contador) + 1;
+    setCookie("contador", contador, 365);
+    p1.textContent =
+      "Según la información de la cookie, esta es su " + contador + "ª visita";
+  } else {
+    contador = 0;
+    if (contador !== "" && contador !== null) {
+      setCookie("contador", contador, 365);
     }
   }
-  
+}
 
 window.addEventListener("load", checkCookie);
-
 
 function setCookie(cname, cvalue, exdays) {
   let d = new Date();
@@ -45,4 +49,32 @@ function getCookie(cname) {
   return "";
 }
 
+///// LOCALSTORAGE ////////////////////////////
 
+const checkLS = () => {
+  if (localStorage.getItem("contador")) {
+    let cont = parseInt(localStorage.getItem("contador"));
+    localStorage.setItem("contador", cont + 1);
+    p2.textContent = "Según el LocalStorage, esta es su " + cont + "ª visita";
+  } else {
+    localStorage.setItem("contador", 0);
+  }
+};
+
+window.addEventListener("load", checkLS);
+
+
+/////// SESSIONSTORAGE /////////////////////////
+
+
+const checkSS = () => {
+    if (sessionStorage.getItem("contador")) {
+      let cont = parseInt(sessionStorage.getItem("contador"));
+      sessionStorage.setItem("contador", cont + 1);
+      p3.textContent = "Según el SessionStorage, esta es su " + cont + "ª visita";
+    } else {
+        sessionStorage.setItem("contador", 0);
+    }
+  };
+  
+  window.addEventListener("load", checkSS);
