0b10385c76dfba0adda2147a88c449537aac005b
issa23i <ipaslop262@iesmartinezm.es>
2022-12-09 13:51:18 +0100

    ejemplo api superheroes

--- U3/ejemploAPI/js/ejemplo1.js
+++ U3/ejemploAPI/js/ejemplo1.js
@@ -1,7 +1,8 @@
 /**
  * 
- */
+ 
  fetch('https://randomuser.me/api/') // 1
- .then(response => response.json()) // 2
+ .then(response => {console.log(response); return response.json()}) // 2
  .then(console.log) // 3
  .catch(error => console.error('algo salio mal' + error)); // 4
+*/
