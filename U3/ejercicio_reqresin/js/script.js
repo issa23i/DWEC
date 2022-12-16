e5202186506a71067d3633d64b092d6272773bfa
issa23i <ipaslop262@iesmartinezm.es>
2022-12-16 13:58:36 +0100

    ejercicio json

--- U3/ejercicio_reqresin/js/script.js
+++ U3/ejercicio_reqresin/js/script.js
@@ -1,23 +1,17 @@
-/**
- * con la api reqres.in obtener para todos los usuarios mostrar el firstname e imagen 
- * (no url, sino la imagen) 
-cargar todos los usuarios de todas las paginas en el html
-(mirar la api para ver cuántqas páginas hay)
-hacer un listado con firtsname e imagen
 
-usar postman para conocer la api
-
-gestión de errores
+// constantes
+const $btn = document.querySelector['button']
+const urlReqres = "https://reqres.in/api/users/";
 
 
- */
 
-let urlReqres = "https://reqres.in/api/users/";
+// acceso API
+
 let user = 1;
 let users = 0;
 let responseOK = false
 let responseOK_ = false
-/** */
+/** Mostrar todos */
 fetch(urlReqres)
 .then(response => {
     responseOK = response.ok
@@ -35,17 +29,20 @@ fetch(urlReqres)
     return data
 })
 .catch((error) => console.log("Error" + error));
-
-const crearTarjeta = (nombre,imagen) => {
-    let $tarjeta = document.createElement('article')
-    document.body.insertAdjacentElement('beforeend',$tarjeta)
-    let $img = document.createElement('img')
-    $img.src = imagen
-    $img.alt = 'Avatar de ' + nombre
-    $tarjeta.insertAdjacentElement('afterbegin',$img)
-    let $p= document.createElement('p')
-    $p.textContent = nombre
-    $tarjeta.insertAdjacentElement('beforeend',$p)
+/** Mostrar el seleccionado */
+const seleccionarUsuario = (e) => {
+    e.preventDefault()
+    $input = document.getElementById("num_usuario")
+    let id = $input.value
+    let urlUser = urlReqres+id
+    console.log(urlUser);
+    fetch(urlUser)
+    .then(json => json.json())
+    .then(data => {
+        nombreUsuario  = data['data']['first_name']
+        console.log(nombreUsuario);
+    })
+    
 }
 
 const llamar = (url) => {
@@ -62,3 +59,40 @@ const llamar = (url) => {
             })
             .catch((error) => console.log("Error" + error));
 }
+
+
+// vista
+
+const crearTarjeta = (nombre,imagen) => {
+    let $tarjeta = document.createElement('article')
+    document.body.insertAdjacentElement('afterbegin',$tarjeta)
+    let $img = document.createElement('img')
+    $img.src = imagen
+    $img.alt = 'Avatar de ' + nombre
+    $tarjeta.insertAdjacentElement('afterbegin',$img)
+    let $p= document.createElement('p')
+    $p.textContent = nombre
+    $tarjeta.insertAdjacentElement('beforeend',$p)
+}
+
+const mostrarUsuario = (email,nombre,apellido,avatar) => {
+    let $divShow = document.querySelector['#show']
+    let $tarjeta = document.createElement('article')
+    $divShow.insertAdjacentElement('afterbegin',$tarjeta)
+    let $img = document.createElement('img')
+    $img.src = avatar
+    $img.alt = 'Usuario seleccionado: ' + nombre
+    $tarjeta.insertAdjacentElement('afterbegin',$img)
+    let $pApellido = document.createElement('p')
+    $pApellido.textContent = apellido
+    $tarjeta.insertAdjacentElement("beforeend",$pApellido)
+    let $pNombre = document.createElement('p')
+    $pNombre.textContent = nombre
+    $tarjeta.insertAdjacentElement("beforeend",$pNombre)
+    let $pEmail = document.createElement('p')
+    $pEmail.textContent = email
+    $tarjeta.insertAdjacentElement("beforeend",$pEmail)
+}
+
+// listeners
+addEventListener('click',seleccionarUsuario)
