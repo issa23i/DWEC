da7945c6b02a63a3074438be23ee5d372858c493
Isabel <isabelpastorlopezcv@gmail.com>
2022-12-19 19:27:13 +0100

    Ejercicio js AJAX json

---
+++ U3/ejercicio_reqresin/js/Usuario.js
@@ -0,0 +1,9 @@
+let Usuario = class {
+    constructor(email,first_name,last_name,avatar){
+        this.email = email
+        this.first_name = first_name
+        this.last_name = last_name
+        this.avatar = avatar
+    }
+
+}
--- U3/ejercicio_reqresin/js/script.js
+++ U3/ejercicio_reqresin/js/script.js
@@ -1,8 +1,10 @@
 
 // constantes
-const $btn = document.querySelector('button')
+const $btn = document.querySelector('button') // boton primer formulario mostrar usuario
+const $btn2 = document.getElementById('boton_objeto') // boton segundo formulario enviar usuario
+const $btn3 = document.getElementById('boton_cambiar') // botón tercero cambiar usuario
+const $btn4 = document.getElementById('boton_eliminar') // último botón de eliminar
 const urlReqres = "https://reqres.in/api/users/";
-
 
 
 // acceso API
@@ -11,6 +13,7 @@ let user = 1;
 let users = 0;
 let responseOK = false
 let responseOK_ = false
+
 /** Mostrar todos */
 fetch(urlReqres)
 .then(response => {
@@ -33,20 +36,28 @@ fetch(urlReqres)
 
 /** Mostrar el seleccionado */
 const seleccionarUsuario = (e) => {
+    responseOK = false
     e.preventDefault()
     $input = document.getElementById("num_usuario")
     let id = $input.value
     let urlUser = urlReqres+id
     fetch(urlUser)
-    .then(json => json.json())
-    .then(data => {
-        let email = data['data']['email']
-        let first_name  = data['data']['first_name']
-        let last_name = data['data']['last_name']
-        let avatar = data['data']['avatar']
-        mostrarUsuario(email,first_name,last_name,avatar)
+    .then(response => {
+        responseOK = response.ok
+        return response.json()
     })
-    
+    .then(data => {
+        if(responseOK){
+            let email = data['data']['email']
+            let first_name  = data['data']['first_name']
+            let last_name = data['data']['last_name']
+            let avatar = data['data']['avatar']
+            mostrarUsuario(email,first_name,last_name,avatar)
+        } else {
+            console.log('No pudo completarse la carga del usuario');
+        }
+    })
+    .catch((error) => console.log('Error : ' + error))
 }
 
 const llamar = (url) => {
@@ -98,5 +109,78 @@ const mostrarUsuario = (email,nombre,apellido,avatar) => {
     $tarjeta.insertAdjacentElement("beforeend",$pEmail)
 }
 
+
+// nuevo usuario
+
+const addUsuario = (e) => {
+
+    // evitar que se envíe el formulario
+    e.preventDefault()
+
+    // recoger valores del formulario
+    let email = document.getElementById('email').value
+    let first_name = document.getElementById('first_name').value
+    let last_name = document.getElementById('last_name').value
+    let avatar = document.getElementById('avatar').value
+
+    // crear el objeto Usuario
+    let newUser = new Usuario(email,first_name,last_name,avatar)
+
+    // guardar el objeto Usuario en Local Storage
+    localStorage.setItem('usuario',newUser)
+
+    // Enviar usuario 
+    fetch("https://reqres.in/api/users", {
+        method: "POST",
+        body: JSON.stringify({ user: newUser})
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+const actualizarUsuario = (e) => {
+    
+    // evitar que se envíe el formulario
+    e.preventDefault()
+
+    // recoger valores del formulario
+    let email = document.getElementById('email_change').value
+
+    // importar usuario del local Storage
+    let usuario = localStorage.getItem('usuario')
+    // cambiar en el objeto usuario, el email
+    usuario.email = email
+
+    // Actualizar email del usuario 
+    fetch("https://reqres.in/api/users/13", {
+        method: "PUT",
+        body: JSON.stringify({ user: usuario})
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+const eliminarUsuario = (e) => {
+    
+    // evitar que se envíe el formulario
+    e.preventDefault()
+
+    // recoger valores del formulario
+    let id = document.getElementById('id_usuario').value
+
+    // eliminar del local storage
+    localStorage.removeItem('usuario')
+
+    // Eliminar
+    fetch(`https://reqres.in/api/users/${id}`, {
+    method: "DELETE"
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
 // listeners
 $btn.addEventListener('click',seleccionarUsuario)
+$btn2.addEventListener('click',addUsuario)
+$btn3.addEventListener('click',actualizarUsuario)
+$btn4.addEventListener('click',eliminarUsuario)
