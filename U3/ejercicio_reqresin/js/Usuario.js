95c65c81695e1dd41fd06d3ea09b935daad80a2e
issa23i <ipaslop262@iesmartinezm.es>
2022-12-20 10:51:38 +0100

    corregido objeto usuario

--- U3/ejercicio_reqresin/js/Usuario.js
+++
@@ -1,9 +0,0 @@
-let Usuario = class {
-    constructor(email,first_name,last_name,avatar){
-        this.email = email
-        this.first_name = first_name
-        this.last_name = last_name
-        this.avatar = avatar
-    }
-
-}
