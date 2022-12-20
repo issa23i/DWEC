c8df91491e694684926417fe521a0cc324d00bd3
Isabel <isabelpastorlopezcv@gmail.com>
2022-12-20 23:31:27 +0100

    terminar ejercicio Json localstorage tabla tareas

--- U3/ejercicio3TODO/js/script.js
+++ U3/ejercicio3TODO/js/script.js
@@ -1,14 +1,15 @@
-// valor para enumerar las tareas
-//localStorage.removeItem('tareas')
-//localStorage.removeItem('indice')
+
+
 const mostrarTabla = () => {
   // si no existe en LocalStorage, lo crea
   let listaTareas = JSON.parse(localStorage.getItem("tareas"));
   if (listaTareas === null) {
     listaTareas = [];
     // se guarda un índice y la lista de tareas en LocalStorage
-    localStorage.setItem("tareas", JSON.stringify(listaTareas));
+    guardarTareas(listaTareas)
     getIndice();
+    // recargar página para que surta efecto
+    window.location.reload()
     //  si existe la lista, se pinta
   } else {
     listaTareas.forEach((tarea) => {
@@ -17,7 +18,6 @@ const mostrarTabla = () => {
         printTarea(tarea);
       }
     });
-    // el índice siempre será el último de la lista
   }
 };
 
@@ -38,63 +38,64 @@ const addTarea = (e) => {
   let listaTareas = JSON.parse(localStorage.getItem("tareas"));
   if (listaTareas === null) {
     listaTareas = [];
-    localStorage.setItem("tareas", JSON.stringify(listaTareas));
+    guardarTareas(listaTareas)
     //  si existe la lista, se inserta y se pinta
   } else {
     listaTareas.push(tarea);
     printTarea(tarea);
   }
   // se guarda la lista de tareas de nuevo en el LocalStorage
-  localStorage.setItem("tareas", JSON.stringify(listaTareas));
+  guardarTareas(listaTareas)
 };
 
-// listeners
+const borrarRegistro = (e) => {
+    let $boton = e.target;
+    let listaTareas = JSON.parse(localStorage.getItem("tareas"));
+    for (let i = 0; i < listaTareas.length; i++) {
+      if (listaTareas[i] !== null) {
+        if (listaTareas[i][0] == $boton.value) {
+          delete listaTareas[i];
+          listaTareas = listaTareas.filter((t) => t !== null);
+          guardarTareas(listaTareas)
+          // recargar página para que aparezca la lista borrada
+          window.location.reload()
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+const printTarea = (tarea) => {
+    let $tbody = document.querySelector("tbody");
+    let $tr = document.createElement("tr");
+    // insertar antes del cierre de la etiqueta tbody
+    $tbody.insertAdjacentElement("afterend", $tr);
+    // Celdas para texto, prioridad, botón borrar
+    let $tdTexto = document.createElement("td");
+    $tdTexto.textContent = tarea[1];
+    let $tdPrioridad = document.createElement("td");
+    $tdPrioridad.textContent = getTextoPrioridad(tarea[2]);
+    // El botón borrar lleva el índice de la tarea
+    let $tdButton = document.createElement("td");
+    let $button = document.createElement("button");
+    let indice = tarea[0];
+    $button.value = indice;
+    $button.textContent = "Borrar";
+    $tdButton.appendChild($button);
+  
+    $tr.appendChild($tdTexto);
+    $tr.appendChild($tdPrioridad);
+    $tr.appendChild($tdButton);
+  
+    // Funcionalidad del botón borrar //// listener ///////////////////////////////////////////////
+    $button.addEventListener("click", borrarRegistro);
+  };
+
+// listeners /////////////////////////////////////////////////////////////////////////////////////
 window.addEventListener("load", mostrarTabla);
 document.querySelector('[type="submit"]').addEventListener("click", addTarea);
 
-const borrarRegistro = (e) => {
-  let $boton = e.target;
-  let listaTareas = JSON.parse(localStorage.getItem("tareas"));
-  for (let i = 0; i < listaTareas.length; i++) {
-    if (listaTareas[i] !== null) {
-      if (listaTareas[i][0] == $boton.value) {
-        delete listaTareas[i];
-        listaTareas = listaTareas.filter((t) => t !== null);
-        localStorage.setItem("tareas", JSON.stringify(listaTareas));
-        // recargar página para que aparezca la lista borrada
-        window.location.reload()
-        return true;
-      }
-    }
-  }
-  return false;
-};
-
-const printTarea = (tarea) => {
-  let $tbody = document.querySelector("tbody");
-  let $tr = document.createElement("tr");
-  // insertar antes del cierre de la etiqueta tbody
-  $tbody.insertAdjacentElement("afterend", $tr);
-  // Celdas para texto, prioridad, botón borrar
-  let $tdTexto = document.createElement("td");
-  $tdTexto.textContent = tarea[1];
-  let $tdPrioridad = document.createElement("td");
-  $tdPrioridad.textContent = tarea[2];
-  // El botón borrar lleva el índice de la tarea
-  let $tdButton = document.createElement("td");
-  let $button = document.createElement("button");
-  let indice = tarea[0];
-  $button.value = indice;
-  $button.textContent = "Borrar";
-  $tdButton.appendChild($button);
-
-  $tr.appendChild($tdTexto);
-  $tr.appendChild($tdPrioridad);
-  $tr.appendChild($tdButton);
-
-  // Funcionalidad del botón borrar
-  $button.addEventListener("click", borrarRegistro);
-};
 
 /**
  * Si el índice no existe lo crea,
@@ -112,3 +113,32 @@ const getIndice = () => {
   }
   return indice;
 };
+
+const ordenarTareas = (tareas) => {
+    // ordenar de mayor prioridad a menor
+    tareas.sort( (t1,t2) => t2[2]-t1[2])
+    // recargar página para que surta efecto
+    window.location.reload()
+}
+
+const guardarTareas = (tareas) => {
+    ordenarTareas(tareas)
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+}
+
+const getTextoPrioridad = (num) => {
+    switch (parseInt(num) ) {
+        case 1:
+            return "Alta"
+            break;
+        case 2:
+            return "Media"
+            break;
+        case 3:
+            return "Baja"
+            break;
+        default:
+            return "No definida"
+            break;
+    }
+}
