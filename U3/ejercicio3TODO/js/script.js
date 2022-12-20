c00145d94fd7feb1b3e22e87c9d446ffefcde665
issa23i <ipaslop262@iesmartinezm.es>
2022-12-20 11:28:31 +0100

    tareas

--- U3/ejercicio3TODO/js/script.js
+++ U3/ejercicio3TODO/js/script.js
@@ -1,2 +1,14 @@
+const mostrarTabla = () => {
+    // si no existe en LocalStorage, lo crea
+    let listaTareas = localStorage.getItem('tareas')
+    if(listaTareas === null){
+        listaTareas = []
+        
+    } else {
+
+    }
+}
 
 
+// listeners
+window.addEventListener('load',mostrarTabla)
