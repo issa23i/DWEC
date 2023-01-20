bf7a514aaee5c83e8002922958a9801ed18c238e
issa23i <ipaslop262@iesmartinezm.es>
2023-01-20 13:56:38 +0100

    modulos listado y entrada

---
+++ todoApp/src/app/todo/todo.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EntradaComponent } from './entrada/entrada.component';
+import { ListadoComponent } from './listado/listado.component';
+
+
+
+@NgModule({
+  declarations: [
+    EntradaComponent,
+    ListadoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodoModule { }
