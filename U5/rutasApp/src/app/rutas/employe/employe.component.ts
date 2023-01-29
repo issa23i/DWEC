c3d61effb1d57fa3a851dd0baf175f6d3d557376
Isabel <isabelpastorlopezcv@gmail.com>
2023-01-29 22:03:55 +0100

    empleado componente

--- U5/rutasApp/src/app/rutas/employe/employe.component.ts
+++ U5/rutasApp/src/app/rutas/employe/employe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { EmployeesComponent } from '../employees/employees.component';
+import { Employe } from '../interfaces/employe';
+import { EmpleadosService } from '../empleados.service';
 
 @Component({
   selector: 'app-employe',
@@ -9,19 +11,28 @@ import { EmployeesComponent } from '../employees/employees.component';
 })
 export class EmployeComponent implements OnInit{
 
-  private _employe: any;
-
-  constructor(private route : ActivatedRoute){}
+  private _employe: Employe = {
+    id: '',
+    name: {
+      first: '',
+      last: ''
+    },
+    company: '',
+    email: ''
+  }
+ 
+  constructor(private activatedRoute : ActivatedRoute, private servicio : EmpleadosService){}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe( (params: ParamMap) => { 
-    })
+    this.activatedRoute.params.subscribe(({id})=>{this._employe.id = id });
+    this._employe = this.servicio.employees.find( e => e.id == this._employe.id)
+
   }
 
-  public get employe(): any {
+  public get employe(): Employe {
     return this._employe;
   }
-  public set employe(value: any) {
+  public set employe(value: Employe) {
     this._employe = value;
   }
 
---
+++ U5/rutasApp/src/app/rutas/interfaces/employe.ts
@@ -0,0 +1,7 @@
+export interface Employe {
+    id : string
+    name : {first : string
+            last : string}
+    company : string;
+    email : string
+}
