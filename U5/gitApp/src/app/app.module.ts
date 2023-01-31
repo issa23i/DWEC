8cf667cdcb799d273233a36f42a1e7364b7228ad
issa23i <isabelpastorlopezcv@gmail.com>
2023-01-31 19:28:12 +0100

    Componentes y exportaciones

--- U5/gitApp/src/app/app.module.ts
+++ U5/gitApp/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
+  ],
+  exports: [
+    SidebarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ U5/gitApp/src/app/gifs/gifs.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BusquedaComponent } from './busqueda/busqueda.component';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { ResultadosComponent } from './resultados/resultados.component';
+import { SidebarComponent } from '../shared/sidebar/sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    BusquedaComponent,
+    GifsPageComponent,
+    ResultadosComponent
+  ],
+  imports: [
+    CommonModule,
+    SidebarComponent
+  ],
+  exports: [
+    BusquedaComponent,
+    GifsPageComponent,
+    ResultadosComponent
+  ]
+})
+export class GifsModule { }
