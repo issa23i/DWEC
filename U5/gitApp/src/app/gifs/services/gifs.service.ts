57948ee96ad89d4d839f3c7a0c75ba22a0f1ce27
issa23i <isabelpastorlopezcv@gmail.com>
2023-02-01 18:08:53 +0100

    to do

--- U5/gitApp/src/app/gifs/services/gifs.service.ts
+++ U5/gitApp/src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Gif } from '../interfaces/gifs';
-import { HttpClientModule, HttpParams } from '@angular/common/http';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs';
+import { HttpClientModule, HttpParams, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -11,23 +11,34 @@ export class GifsService {
       LocalStorage en gifs.service.ts 
     */
 
+  
+  private apiKey: string = 'DjNFYau3pbGLWJ2BlxVtdFHiNiMHA5Vu';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs'; //URL
+  private _historial: string[] = []; // historial de búsquedas • Variable para guardar los resultados.
+  public resultados: Gif[] = []; // interfaz Gif • Variable para guardar las búsquedas.
+
+  gethistorial() {}
+
+  constructor(private http: HttpClient) {}
+  
+
   /**
-   * Dentro del servicio:
-      • Variable para guardar las búsquedas.
-      • Variable para guardar los resultados.
       • Método para realizar búsquedas y limitar a 10 elementos únicos (sin importar mayúsculas
           o minúsculas). Usaremos HttpClient para implementar el uso de la API (también se podría
           hacer con fetch)
    */
-  private apiKey: string = 'DjNFYau3pbGLWJ2BlxVtdFHiNiMHA5Vu';
-  private servicioUrl: string = 'https://api.giphy.com/v1/gifs'; //URL
-  private _historial: string[] = []; //Guarda el historial de búsquedas
-  public resultados: Gif[] = []; //Hacemos uso de la interfaz Gif
-
-  gethistorial() {}
-
-  constructor(private http: HttpClientModule) {}
-  buscarGifs(query: string = '') {}
+  buscarGifs(query: string = '') {
+    //Creamos un objeto para los parámetros de llamada a la API
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+    this.http
+      .get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params,}) //SearchGifsResponse se obtiene de la interfaz
+      .subscribe((resp) => {
+        this.resultados = resp.data;
+      });
+  }
   /*
 fetch
 ("https://api.giphy.com/v1/gifs/search?api_key=LsRGOaQxDIOkxXYmt1v8yQx7fm
@@ -39,17 +50,4 @@ console.log(data);
 })
 })
 */
-  //Creamos un objeto para los parámetros de llamada a la API
-  /** 
-const params = new HttpParams()
-.set('api_key', this.apiKey)
-.set('limit', '10')
-.set('q', query );
-this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, {
-params } )//SearchGifsResponse se obtiene de la interfaz
-.subscribe( ( resp ) => {//resp es un Observable, con más
-funcionalidades que las Promesas
-this.resultados = resp.data;
-});
-}*/
 }
