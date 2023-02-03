50ab0af5cdc600ded6bccb59813e4c47cdb07bf3
issa23i <isabelpastorlopezcv@gmail.com>
2023-02-03 13:58:24 +0100

    rutas continuar

--- U5/paisesApp/src/app/app-routing.module.ts
+++ U5/paisesApp/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from '../../../angular-basico-bootstrap-mio-base/src/app/app.component';
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
