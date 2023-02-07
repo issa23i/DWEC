d9810583ed0f548864f7e420d71cb9e7418612e1
issa23i <isabelpastorlopezcv@gmail.com>
2023-02-07 10:06:55 +0100

    sidebar routerlinkactiveoptions de RouterModule

--- U5/paisesApp/src/app/app.module.ts
+++ U5/paisesApp/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
--- U5/paisesApp/src/app/shared/shared.module.ts
+++ U5/paisesApp/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SidebarComponent
