3f81dd54951ae287e513c34534a86f7092ace65e
issa23i <isabelpastorlopezcv@gmail.com>
2023-02-09 22:07:34 +0100

    por capital

--- U5/paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
+++ U5/paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pais } from '../../interfaces/pais';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -7,4 +9,51 @@ import { Component } from '@angular/core';
 })
 export class PorCapitalComponent {
 
+  private _paises: Pais[] = [];
+  private _termino: string = '';
+  private _hayError: boolean = false;
+  
+
+  constructor(private paisServ : PaisService){}
+
+  buscar(): void {
+    if ( this._termino !== '') {
+      this._hayError = false;
+      this.paisServ.buscarCapital(this._termino)
+        .subscribe({
+          next: (resp) => {
+            this.paisServ.paises = resp
+            this._paises = resp
+            //console.log(resp)
+          },
+          error: (err) => {
+            this._hayError = true
+            this.paisServ.paises = []
+            this._paises = []
+            console.log("ERROR " + err)
+          }
+        })
+      this._termino = '';
+    }
+  }
+
+  
+  public get hayError(): boolean {
+    return this._hayError;
+  }
+  public set hayError(value: boolean) {
+    this._hayError = value;
+  }
+  public get paises(): Pais[] {
+    return this._paises;
+  }
+  public set paises(value: Pais[]) {
+    this._paises = value;
+  }
+  public get termino(): string {
+    return this._termino;
+  }
+  public set termino(value: string) {
+    this._termino = value;
+  }
 }
