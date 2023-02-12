c88f7492c9410d166b187148bf5f34aef42fca68
issa23i <isabelpastorlopezcv@gmail.com>
2023-02-12 19:27:31 +0100

    Interface Infopagina

---
+++ U5/plantillaToAngular/src/app/interfaces/info-pagina.ts
@@ -0,0 +1,13 @@
+export interface InfoPagina {
+    titulo?:         string;
+    email?:          string;
+    nombre_corto?:   string;
+    pagina_autor?:   string;
+    facebook?:       string;
+    twitter?:        string;
+    instagram?:      string;
+    tublr?:          string;
+    linkedin?:       string;
+    github?:         string;
+    equipo_trabajo?: any[];
+}
--- U5/plantillaToAngular/src/app/services/info-pagina.service.ts
+++ U5/plantillaToAngular/src/app/services/info-pagina.service.ts
@@ -1,20 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { InfoPagina } from '../interfaces/info-pagina';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
+  private _infoPagina: InfoPagina = {};
+  
+
   constructor(private http: HttpClient) {
     console.log('Servicio InfoPagina');
     this.http.get('assets/info.json')
     .subscribe((resp: any) => {
-      console.log(resp['twitter']);
-      
+      this.infoPagina = resp
+      console.log(this.infoPagina['twitter']);
     },
     (err) => {
       console.error('Ocurrió un erro al acceder a la información ', err)
     });
+    }
+
+    public get infoPagina(): InfoPagina {
+      return this._infoPagina;
+    }
+    public set infoPagina(value: InfoPagina) {
+      this._infoPagina = value;
     }
 }
