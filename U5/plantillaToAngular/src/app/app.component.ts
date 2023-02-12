e88b99bb6cdbb014ae09217d72960b4ae1082187
issa23i <isabelpastorlopezcv@gmail.com>
2023-02-12 19:13:54 +0100

    Usando un JSON

--- U5/plantillaToAngular/src/app/app.component.ts
+++ U5/plantillaToAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InfoPaginaService } from './services/info-pagina.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'plantillaToAngular';
+  constructor(private infoPaginaService: InfoPaginaService) {}
 }
---
+++ U5/plantillaToAngular/src/app/services/info-pagina.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  constructor(private http: HttpClient) {
+    console.log('Servicio InfoPagina');
+    this.http.get('assets/info.json').subscribe((resp: any) => {
+    //console.log(resp);
+    console.log(resp['twitter']);
+    //console.log(resp.twitter);
+    });
+    }
+}
