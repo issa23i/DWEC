f8c308bdf6c099c1c75a4b0862389a612a43e251
issa23i <isabelpastorlopezcv@gmail.com>
2023-02-23 18:27:36 +0100

    Estudiando pipes

--- U5/fechasApp/src/app/fecha/datepipe/datepipe.component.ts
+++ U5/fechasApp/src/app/fecha/datepipe/datepipe.component.ts
@@ -6,6 +6,7 @@ import { Component} from '@angular/core';
   styleUrls: ['./datepipe.component.css']
 })
 export class DatepipeComponent {
-  
+  numero : number = 4.7
+  frase : string = 'Cuatro patas tiene un gato'
   fecha: Date = new Date(1988, 3, 15, 12, 30, 15, 46); //15 de Abril del 1988 12:30:15
 }
