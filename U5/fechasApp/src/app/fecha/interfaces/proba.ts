a0532dafd4d5627b1afc65ea497a0c331db68983
issa23i <isabelpastorlopezcv@gmail.com>
2023-02-23 21:36:45 +0100

    pruebas

---
+++ U5/fechasApp/src/app/fecha/interfaces/proba.ts
@@ -0,0 +1,57 @@
+interface RootObject {
+    drinks: Drink[];
+  }
+  
+  interface Drink {
+    idDrink: string;
+    strDrink: string;
+    strDrinkAlternate?: any;
+    strTags?: string;
+    strVideo?: any;
+    strCategory: string;
+    strIBA?: string;
+    strAlcoholic: string;
+    strGlass: string;
+    strInstructions: string;
+    strInstructionsES?: any;
+    strInstructionsDE: string;
+    strInstructionsFR?: any;
+    strInstructionsIT: string;
+    'strInstructionsZH-HANS'?: any;
+    'strInstructionsZH-HANT'?: any;
+    strDrinkThumb: string;
+    strIngredient1: string;
+    strIngredient2: string;
+    strIngredient3: string;
+    strIngredient4?: string;
+    strIngredient5?: any;
+    strIngredient6?: any;
+    strIngredient7?: any;
+    strIngredient8?: any;
+    strIngredient9?: any;
+    strIngredient10?: any;
+    strIngredient11?: any;
+    strIngredient12?: any;
+    strIngredient13?: any;
+    strIngredient14?: any;
+    strIngredient15?: any;
+    strMeasure1: string;
+    strMeasure2: string;
+    strMeasure3?: string;
+    strMeasure4?: any;
+    strMeasure5?: any;
+    strMeasure6?: any;
+    strMeasure7?: any;
+    strMeasure8?: any;
+    strMeasure9?: any;
+    strMeasure10?: any;
+    strMeasure11?: any;
+    strMeasure12?: any;
+    strMeasure13?: any;
+    strMeasure14?: any;
+    strMeasure15?: any;
+    strImageSource?: string;
+    strImageAttribution?: string;
+    strCreativeCommonsConfirmed: string;
+    dateModified: string;
+  }
